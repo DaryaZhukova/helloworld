2be930ad6b4d94598991f38caf6c3d1ff2438246
Darya Zhukova <Darya_Zhukova@epam.com>
2018-07-29 19:27:20 +0100

    push/pull script

---
+++ all.groovy
@@ -0,0 +1,62 @@
+@Grapes(
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
+)
+import groovyx.net.http.RESTClient
+import org.apache.http.entity.*
+import hudson.model.*
+def encodeTarFile( Object data ) throws UnsupportedEncodingException {
+   def entity = new FileEntity( (File) data, "application/tar.gz" );
+   entity.setContentType( "application/tar.gz" );
+   return entity
+   }
+String[] parse_gav() {
+        def pom = new XmlSlurper().parse(new File('/opt/jenkins/workspace/build_helloworld/pom.xml'))
+        def gavs = []
+        gavs.add(pom.groupId)
+        gavs.add(pom.artifactId)
+        gavs.add(pom.version)
+        return gavs
+}
+def upload() {
+   def path = System.getenv('WORKSPACE');
+   def buildnumber = System.getenv('BUILD_NUMBER')
+   def repo = "maven-helloworld"
+   def gav = parse_gav()
+   def file = new File("${path}/artifact.tar.gz")
+   def rest = new RESTClient( "http://192.168.17.4/repository/${repo}")
+        rest.auth.basic 'admin', 'admin123'
+        rest.encoder.'application/tar.gz' = this.&encodeTarFile
+        rest.put( path:  "http://192.168.17.4/repository/${repo}/${gav[0]}/${gav[1]}/${buildnumber}/${gav[1]}-${buildnumber}.tar.gz", body: file, requestContentType: 'application/tar.gz')
+
+}
+
+
+def download(def path) {
+   name = path.split("/")[6]
+   def repo = "maven-helloworld"
+   def rest = new RESTClient( "http://192.168.17.4/repository/${repo}")
+        rest.auth.basic 'admin', 'admin123'
+        def resp = rest.get( path:  "${path}")
+   new File("./${name}.war") << resp.data
+}
+
+def cli = new CliBuilder(usage:'ls')
+import org.apache.commons.cli.Option
+cli.with
+{
+   h(longOpt: 'help', 'Usage Information', required: false)
+   p(longOpt: 'path', 'ARTIFACT_NAME', args: 1, required: false)
+   c(longOpt: 'option', 'push/pull', args: 1, required: true)
+}
+
+def options = cli.parse(args)
+println options.p
+println options.c
+if (options.c=='pull') {   
+        download(options.p)
+}
+if (options.c=='push') {
+        upload()
+}
+
+
